709f286e2c110ee3e0cafca71d70af8c971a629f
leeheeweon <ggoddii@naver.com>
2021-11-14 14:06:46 +0900

    setting

---
+++ src/main/java/com/kaoni/theCulureOfLearning/BoardTestRepoApplication.java
@@ -0,0 +1,13 @@
+package com.kaoni.theCulureOfLearning;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BoardTestRepoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BoardTestRepoApplication.class, args);
+	}
+
+}
